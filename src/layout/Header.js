20546bab3980ecb5b2fb14033d756ee4173e64cf
Abhishek Kumar <hind.tigerabhisingh@gmail.com>
2020-05-28 16:24:58 +0530

    header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../assets/css/header.css';
+import callLogo from '../assets/imgs/phone-alt-solid.svg';
 
 function Header() {
   return (
-    <div style={{textAlign:"center", backgroundColor:'red',height:'50px',margin:'2px'}}>
-      Header Component
+    <div className="maindiv">
+      <div className="header">
+        <a className="active" href="./home">Home</a>
+        <a href="./product">Product</a>
+        <a href="./pricing">Pricing</a>
+        <a href="./help">Help</a>
+        <a href="./login">Login</a>
+
+        <button className="trail">FREE TRAIL</button>
+
+        <div className="calldiv">
+          <img src={callLogo} alt="Call Icon" className="call" />
+        </div>
+
+
+      </div>
     </div>
   )
 }
-
 
 export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import HomeImage from '../assets/imgs/home.jpg';
 import Header from '../layout/Header'
-import Footer from '../layout/Footer'
-import MiddleSection from '../components/MiddleSection'
+// import Footer from '../layout/Footer'
+// import MiddleSection from '../components/MiddleSection'
 
 function Home() {
   return (
     <>
-      <Header />
+    <Header />
+      <img src={HomeImage} alt="Home" style={{paddingTop:"52px"}}/>
+      {/* <Header />
       <MiddleSection />
-      <Footer />
+      <Footer /> */}
     </>
   )
 }
